806424117d6e448f46dbb664ebe19fea9d385039
Alessandro <alessandro.campanello@gmail.com>
2024-11-28 17:25:11 +0100

    Added test environment and base for unit testing

--- tests/create_tables.py
+++ tests/create_tables.py
@@ -1,5 +1,7 @@
-# Importa la libreria dotenv per caricare le variabili di ambiente dal file .env
+import sys
+sys.path.append('..')
 from db.models import Base, Product, Store, DateDim, SalesFact  # Importa Base e le classi dei modelli
+# Importa la libreria dotenv per caricare le variabili di ambiente dal file .env
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Date, Float
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,16 +12,16 @@ import os
 load_dotenv()
 
 # Recupera la variabile di ambiente DATABASE_URL
-DATABASE_URL = os.getenv("DATABASE_URL")
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL non trovata nel file .env")
+if TEST_DATABASE_URL is None:
+    raise ValueError("TEST_DATABASE_URL non trovata nel file .env")
 
 # Crea una connessione al database
-engine = create_engine(DATABASE_URL)
+engine = create_engine(TEST_DATABASE_URL)
 
 # Creare tutte le tabelle nel database
 Base.metadata.create_all(engine)
 
-print("Tabelle create nel database con successo.")
+print("Tabelle create nel database di test con successo.")
 
